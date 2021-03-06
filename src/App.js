f23bb158e17afa0af170ced78a3c774014ee4384
Michał Buda <michalbuda999@gmail.com>
2021-03-06 16:43:58 +0100

    added menu page components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Button from '../src/components/Button.js'
+import { ReactComponent as Logo } from '../src/assets/images/logo.svg'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello meal-list!</h1>
+      <span className="logo">
+        <Logo />
+      </span>
+      <Button name="Go to meal list" />
+      <Button name="Saved meals" />
     </div>
+
   );
 }
 
